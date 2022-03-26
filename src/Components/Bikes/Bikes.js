ec53bb8c94291bd401ee0c1ff6d7e016b2d293ea
abidhossain2 <tanvirabid229@gmail.com>
2022-03-26 22:07:17 +0600

    random choose added

--- src/Components/Bikes/Bikes.js
+++ src/Components/Bikes/Bikes.js
@@ -8,18 +8,28 @@ const Bikes = () => {
         .then(res => res.json())
         .then(data => setBikes(data))
     } ,[])
-    let [product, setProduct] = useState([])
+    const [product, setProduct] = useState([])
     const addToCart = (bike) => {
         const newProduct = [...product,bike]
         const newItem = [...new Set(newProduct)]
         setProduct(newItem);
+        if(newItem.length > 4){
+            
+        }
+    }
+    const chooseItem = (bike) => {
+        const newProduct = [...product,bike]
+        const newItem = [...new Set(newProduct)]
+        setProduct(newItem);
+        const ans = newProduct[Math.floor(Math.random() * newProduct.length)].name;
+        let result = document.getElementById('choose');
+        result.innerText = ans
     }
     const deleteItem = () => {
         const newProduct = [];
         setProduct(newProduct)
+        document.getElementById('choose').innerText = "";
     }
-
-
     return (
         <div className='store-bike'>
         <div className='bikes-container'>
@@ -32,19 +42,26 @@ const Bikes = () => {
             }
         </div>
         <div className='order-container'>
-                  Order Summary
             <div>
-               <div>
-               {
-                   product.map(bikeItem =>
-                    <div key={bikeItem.id} style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-                        <img style={{width:90, borderRadius:9, marginRight:30}} src={bikeItem.img} alt="" />
-                    <h5 key={bikeItem.id}>{bikeItem.name}</h5>
+                <p>Order Summary</p>
+                <p id='choose' style={{marginTop:60, fontSize:14, background:'aqua', padding:10, borderRadius:9}}></p>
+            </div>
+
+            <div className='single-bike'>
+            {
+                product.map(bikeItem =>
+                    <div key={bikeItem.id} style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop:10}}>
+                    <img style={{width:90, borderRadius:9, marginRight:30}} src={bikeItem.img} alt="" />
+                    <h6 key={bikeItem.id}>{bikeItem.name}</h6>
                     </div>
                     )
-                }
-                <button onClick={deleteItem}>Clear Item</button>
-               </div>
+            }
+            <div className='deleteBtn'>
+            <button onClick={deleteItem}>Clear Item</button>
+            </div>
+            <div className='chooseBtn'>
+            <button onClick={chooseItem}>Click for lucky</button>
+            </div>
             </div>
         </div>
         </div>
